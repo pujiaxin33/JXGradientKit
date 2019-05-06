f597f29447aede256f430f68851282da606cda26
pujiaxin33 <317437084@qq.com>
2019-05-06 17:33:39 +0800

    示例代码优化

--- JXGradientKit/ViewController.swift
+++ JXGradientKit/ViewController.swift
@@ -14,9 +14,15 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let firstColor = UIColor(red: 0/255.0, green: 185/255.0, blue: 172/255.0, alpha: 1).cgColor
-        let secondColor = UIColor(red: 86/255.0, green: 126/255.0, blue: 177/255.0, alpha: 1).cgColor
-        gradientView.gradientLayer.colors = [firstColor, secondColor]
+        //直接通过gradientLayer配置
+        let firstColor = UIColor(red: 0/255.0, green: 185/255.0, blue: 172/255.0, alpha: 1)
+        let secondColor = UIColor(red: 86/255.0, green: 126/255.0, blue: 177/255.0, alpha: 1)
+        gradientView.gradientLayer.colors = [firstColor.cgColor, secondColor.cgColor]
+
+        //通过便利属性配置
+//        gradientView.direction = JXGradientLayerDirection.leftToRight.rawValue
+//        gradientView.startColor = firstColor
+//        gradientView.endColor = secondColor
     }
 
 
